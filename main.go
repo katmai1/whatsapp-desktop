3a1e49ac1b582abfae6e4d9fc1dc617c3a7690c9
katmai <katmai.mobil@gmail.com>
2018-12-14 19:31:34 +0100

    qt implementado pero la version es demasiado antigua para whatsapp web

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-import "github.com/zserge/webview"
+import (
+	// "github.com/zserge/webview"
+	"github.com/visualfc/goqt/ui"
+)
 
 func main() {
+	s := make([]string, 1)
+	winn := ui.NewApplication(s)
+	winn.SetAttributeWithAttributeOn(ui.Qt_AA_MSWindowsUseDirect3DByDefault, true)
+	// winn.
+	loader := ui.NewUiLoader()
+	w := loader.Load(ui.NewFileWithName("form.ui"))
+	// winn.SetAttribute(ui.Qt_AA_MSWindowsUseDirect3DByDefault)
+	// winn.SetAttributeWithAttributeOn(ui.Qt_AA_, true)
 	// Open wikipedia in a 800x600 resizable window
-	webview.Open("Whatsapp Desktop",
-		"https://web.whatsapp.com/", 800, 600, true)
+	// webview.Open("Whatsapp Desktop",
+	// "https://web.whatsapp.com/", 800, 600, true)
+	w.Show()
+	winn.Exec()
 }
